b8893eb77200b208d6c353c04176b6b77b726794
AustinReed-1 <austindaveyreed@gmail.com>
2023-12-06 16:20:02 +0000

    Add essential mouse config files for readPosition (density.smk) pipeline as well as replicationTiming files for mice.

--- data/readPosition/extract-start-sites.sh
+++ data/readPosition/extract-start-sites.sh
@@ -3,22 +3,26 @@
 #A fairly simple script which takes an indexed, co-ordinate sorted bam file as input, marks and remove duplicates, and then produces a table of reads including the reference and genomic start site for each read
 
 # Original author: Thomas Bradley
-# Modified by: Michael Schneider
+# Modified by: Michael Schneider 
+# Later modifications by: Austin Reed
 
 input_bam=$1
 relevant_reference_sequences=$2
 workdir=$(dirname "$0")
 #echo $workdir
 
+# sort bam file
+samtools sort -o ${input_bam}.sorted ${input_bam}
+
 # mark duplicates
-picard MarkDuplicates I=${input_bam} O=${input_bam}.deduplicated M=${input_bam}.duplicate_metrics.txt REMOVE_DUPLICATES=true VALIDATION_STRINGENCY=LENIENT
+picard MarkDuplicates I=${input_bam}.sorted O=${input_bam}.deduplicated M=${input_bam}.duplicate_metrics.txt REMOVE_DUPLICATES=true VALIDATION_STRINGENCY=LENIENT
 # java -jar ./picard.jar MarkDuplicates I=${input_bam} O=${input_bam}.deduplicated M=${input_bam}.duplicate_metrics.txt REMOVE_DUPLICATES=true
 
 # convert chromosome/contig co-ordinates to genome co-ordinates
 samtools view -H ${input_bam}.deduplicated | grep '^@SQ' | awk '{FS="\t"}{OFS="\t"}{print $2,$3}'  > ${input_bam}.reference_sizes.txt
 
 #
-if [ $(samtools view -c -f 1 ${input_bam}) = "0" ]; then
+if [ $(samtools view -c -f 1 ${input_bam}.sorted) = "0" ]; then
     # single-end reads
     echo "single-end reads"
     # get table from deduplicated bam
@@ -37,6 +41,7 @@ fi
 Rscript --vanilla $workdir/get_table.R ${input_bam}.reference_sizes.txt ${relevant_reference_sequences} ${input_bam}.deduplicated.read_start_sites.txt ${input_bam%.bam}.position.tsv
 
 # clean up
+rm -f ${input_bam}.sorted
 rm -f ${input_bam}.deduplicated
 rm -f ${input_bam}.duplicate_metrics.txt
 rm -f ${input_bam}.deduplicated.read_start_sites.txt
